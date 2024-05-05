177e516ae735ee22401ec292a3b1e19c8cd508f6
Pritam <pritam.hdc@gmail.com>
2024-05-05 23:39:52 +0530

    removeElement.py array prog comment added

--- removeElement.py
+++ removeElement.py
@@ -1,3 +1,6 @@
+#Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+# The order of the elements may be changed. Then return the number of elements in nums which are 
+# not equal to val.
 class Solution(object):
     
     def removeElement(self, nums, val):
