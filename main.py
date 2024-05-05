7e880f53beafeed105bb9ea5bfd6c7eb5ea4ea9b
Pritam <pritam.hdc@gmail.com>
2024-05-05 11:10:44 +0530

    final feature1

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import pandas as pd
 print("hello")
 df=pd.read_csv('test.csv')
 print(df.to_string()) 
-<<<<<<< HEAD
 print("New Feature added test2")
-=======
 print("New Feature added test1")
->>>>>>> main
 
 #spark=SparkSession.builder.appname('Practise').getOrCreate()
 
