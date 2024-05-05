33f9f720b69a5935c9dea88f4cf23f5897095957
Pritam <pritam.hdc@gmail.com>
2024-05-05 10:59:02 +0530

    keep only feature1 changes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import pandas as pd
 print("hello")
 df=pd.read_csv('test.csv')
-print(df.to_string()) 
-print("New Feature added test1")
+print(df.to_string())
+print("New Feature added test2")
 
 #spark=SparkSession.builder.appname('Practise').getOrCreate()
 
